128923ae304eaed5614a946e76081cdc21a48a16
greenhornsr <srollins11279@gmail.com>
2019-05-15 14:29:59 -0700

    imported applyMiddleware and compose - for redux tools -

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reducer from './reducer';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducer)
+// middleware
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
