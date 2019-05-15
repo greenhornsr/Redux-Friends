166948861e902889f5f688385462e7f902e62307
greenhornsr <srollins11279@gmail.com>
2019-05-15 14:23:28 -0700

    built out initial reducer, connected to app to Store

---
+++ friends/src/reducer/index.js
@@ -0,0 +1,18 @@
+
+
+const initialState = {
+    friends: [],
+    deletingFriend: false,
+    fetchingFriends: false,
+    loggingIn: false,
+    savingFriends: false,
+    updatingFriend: false,
+    error: null
+}
+
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        
+    }
+
+}
